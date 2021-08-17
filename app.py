cdb970b7c20136f8e31505e29bfa1c4c571e7614
AndyB-WHG <andrew.bond2@gmail.com>
2021-08-17 08:07:53 +0000

    Feat:  'Login' page and functionality added.

--- app.py
+++ app.py
@@ -55,6 +55,32 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if user exists in 'sim_setup_world / users' database (MongoDB)
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check hashed password matches password in database
+            if check_password_hash(
+              existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+
+            else:
+                # if password does not match
+                flash("Incorrect username and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect username and/or password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
