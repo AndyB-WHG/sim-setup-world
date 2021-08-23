fee56dd7f36997ab80540af7bd18c29bfa1fa490
AndyB-WHG <andrew.bond2@gmail.com>
2021-08-23 19:59:11 +0000

    Submit-setup page listener now determines the name of the Sim selected.

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,17 @@ $(document).ready(function () {
 
 
 $("#sim-name").on('change', function() {
-    console.log($(this));
-    console.log("option selected!")
+    // the following line of code was found at https://www.codegrepper.com/code-examples/javascript/how+to+get+the+option+text+from+dropdown+with+option+value+javascript
+    var sim_name = this.options[this.selectedIndex].text;
+    console.log(sim_name);    
     
+    // $("#pageTitle").html("<h1>Hello, World!</h1>");
+
+    $("#car-name").html("<option value='' disabled selected></i>Select Car</option>" +
+        "{% set car_count = {'total': 0} %} + 
+        "{%- for car in cars -%}" +
+        "{% if car.sim_name =" + sim_name + "%}" +
+            "{% if car_count.update({'total': car_count.total + 1}) %}{% endif %}" +
+                "<option value='{{count.total}}'>{{ car.car_name }}</option>" +
+        "{%- endfor -%}");
 });
