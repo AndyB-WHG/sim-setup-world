ddbee1e767c9d80693dcf7795c40ae2f4f141907
AndyB-WHG <andrew.bond2@gmail.com>
2021-09-05 20:00:00 +0000

    Fix:  'Add seetings' button added - not yet working properly.

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,7 @@ $("#sim_name").on('change', function() {
     // the following line of code was found at https://www.codegrepper.com/code-examples/javascript/how+to+get+the+option+text+from+dropdown+with+option+value+javascript
     var sim_name = this.options[this.selectedIndex].text;
     console.log(sim_name);
-    document.getElementById('submit-setup-button-div').innerHTML = '<div class="col s12 center-align"><button id="submit-setup-button" type="submit" class="btn-large light-blue text-shadow">Submit<i class="fas fa-plus-square right"></i></button></div>'
+    document.getElementById('submit-setup-button-div').innerHTML = '<div class="col s12 center-align"><button id="submit-setup-button" type="submit" class="btn-large light-blue text-shadow">Automated Button Click<i class="fas fa-plus-square right"></i></button></div>'
     document.getElementById('submit-setup-button').click();
 });
 
@@ -21,3 +21,9 @@ $("#car_name").on('change', function() {
     var car_name = this.options[this.selectedIndex].text;
     console.log(car_name);
 });
+
+$("#enter-settings-button").onclick, function() {
+    console.log("Settings section ready to be rendered.")
+    document.getElementById("settings-section").innerHTML = '<h1>Settings Go Here!</h1>'
+};
+
