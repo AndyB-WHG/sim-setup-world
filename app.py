36f937dff5f07508dced5f43b0d7217569d17a8a
AndyB-WHG <andrew.bond2@gmail.com>
2021-09-19 08:30:04 +0000

    Fix:  Added a check to endure user is logged in before being able to access the 'View My Setups' and 'Submit Setup' pages.

--- app.py
+++ app.py
@@ -121,26 +121,32 @@ def logout():
 
 @app.route("/submit_setup_part1", methods=["GET", "POST"])
 def submit_setup_part1():
-    if request.method == "POST":
-        # Take user's 'sim' selection and inject cars and tracks relating to
-        # the sim in question for user to choose from.
-        sim_name = request.form["sim_name"]
-        print("150: Selected Sim: ", sim_name)
-        cars = list(mongo.db.car_list.find(
-            {"sim_name": request.form.get("sim_name")}).sort("car_name"))
-        tracks = list(mongo.db.tracks.find(
-            {"sim_name": request.form.get("sim_name")}).sort("track_name"))
-        print("180: Car and Track options lists loaded - user needs to" +
-              "select car and track")
-        return render_template(
-            "submit_setup_part2.html",
-            sim_name=sim_name, cars=cars, tracks=tracks)
+    try: 
+        if session["user"]:
+            if request.method == "POST":
+                # Take user's 'sim' selection and inject cars and tracks relating to
+                # the sim in question for user to choose from.
+                sim_name = request.form["sim_name"]
+                print("150: Selected Sim: ", sim_name)
+                cars = list(mongo.db.car_list.find(
+                    {"sim_name": request.form.get("sim_name")}).sort("car_name"))
+                tracks = list(mongo.db.tracks.find(
+                    {"sim_name": request.form.get("sim_name")}).sort("track_name"))
+                print("180: Car and Track options lists loaded - user needs to" +
+                    "select car and track")
+                return render_template(
+                    "submit_setup_part2.html",
+                    sim_name=sim_name, cars=cars, tracks=tracks)
 
-    # Render the 'Submit Setup' page and inject 'Sim' options for user to select from
-    sims = list(mongo.db.sims.find().sort("sim_name"))
-    print("100: Select Sim")
-    return render_template(
-        "submit_setup_part1.html", sims=sims)
+            # Render the 'Submit Setup' page and inject 'Sim' options for user to select from
+            sims = list(mongo.db.sims.find().sort("sim_name"))
+            print("100: Select Sim")
+            return render_template(
+                "submit_setup_part1.html", sims=sims)
+
+    except:
+        flash("Please login to submit a setup")
+        return redirect(url_for("home"))
 
 
 # Submit car and track choices
@@ -217,37 +223,43 @@ def submit_setup_part3():
         return render_template("home.html")
 
 
-# Initial 'My Setups' page 
+# Initial 'My Setups' page
 @app.route("/my_setups_part1/", methods=["GET", "POST"])
 def my_setups_part1():
-    if request.method == "POST":
-        # Take user's choice of sim and retrieve relevant car and track
-        # lists for the sim in question.
-        sim_name = request.form["sim_name"]
-        print("150: Selected Sim: ", sim_name)
-        cars = list(mongo.db.car_list.find(
-            {"sim_name": request.form.get("sim_name")}).sort("car_name"))
-        tracks = list(mongo.db.tracks.find(
-            {"sim_name": request.form.get("sim_name")}).sort("track_name"))
-        print("180: Car and Track options lists loaded - user needs to" +
-              "select car and track")
+    try:
+        if session["user"]:
+            if request.method == "POST":
+                # Take user's choice of sim and retrieve relevant car and track
+                # lists for the sim in question.
+                sim_name = request.form["sim_name"]
+                print("150: Selected Sim: ", sim_name)
+                cars = list(mongo.db.car_list.find(
+                    {"sim_name": request.form.get("sim_name")}).sort("car_name"))
+                tracks = list(mongo.db.tracks.find(
+                    {"sim_name": request.form.get("sim_name")}).sort("track_name"))
+                print("180: Car and Track options lists loaded - user needs to" +
+                    "select car and track")
 
-        # Inject relevant cars and tracks and render on page 2 for user
-        # to select from.
-        return render_template(
-            "my_setups_part2.html",
-            sim_name=sim_name, cars=cars, tracks=tracks)
+                # Inject relevant cars and tracks and render on page 2 for user
+                # to select from.
+                return render_template(
+                    "my_setups_part2.html",
+                    sim_name=sim_name, cars=cars, tracks=tracks)
 
-    # Render initial 'User Setups' page, and inject 'Sim' list for user to
-    # choose from if they so wish.
-    sims = list(mongo.db.sims.find().sort("sim_name"))
+            # Render initial 'User Setups' page, and inject 'Sim' list for user to
+            # choose from if they so wish.
+            sims = list(mongo.db.sims.find().sort("sim_name"))
 
-    # Get a list of all the user's setups to be placed at the bottom of the
-    # page ready for selection.
-    user_setups = list(mongo.db.setups.find({"created_by": session["user"]})
-                                      .sort("_id"))
-    return render_template(
-        "my_setups_part1.html", sims=sims, user_setups=user_setups)
+            # Get a list of all the user's setups to be placed at the bottom of the
+            # page ready for selection.
+            user_setups = list(mongo.db.setups.find({"created_by": session["user"]})
+                                            .sort("_id"))
+            return render_template(
+                "my_setups_part1.html", sims=sims, user_setups=user_setups)
+
+    except:
+        flash("Please login to submit a setup")
+        return redirect(url_for("home"))
 
 
 # 'My Setups' car and track selection.
@@ -283,46 +295,51 @@ def my_setups_part3():
 
 @app.route("/edit_setup/<setup_id>", methods=["GET", "POST"])
 def edit_setup(setup_id):
-    if request.method == "POST":
-        sim_name = request.args.get('sim_name', None)
-        car_name = request.args.get('car_name', None)
-        track_name = request.args.get('track_name', None)
-        print("Part 6 : ", sim_name)
-        print("Part 6 : ", car_name)
-        print("Part 6 : ", track_name)
-        param_dict_list = list(mongo.db.sim_settings_parameters.find(
-                           {"sim_name": sim_name}).sort("order_number"))
-        print("Part 6 Parameters : ", param_dict_list)
-        print("Length of Parameters List is : ", len(param_dict_list))
-        update_dict = {}
-        dateTimeObj = datetime.now()
-        # the following line of code borrowed from "https://thispointer.com/python-how-to-convert-datetime-object-to-string-using-datetime-strftime/"
-        # to get the current date and time.
-        timestampStr = dateTimeObj.strftime("%Y %m %d - %H:%M:%S")
-        print('Current Timestamp PT6 : ', timestampStr)
-        for param_dict in param_dict_list:
-            parameter_name = param_dict["param"]
-            update_dict[parameter_name] = request.form.get(parameter_name)
-        update_dict["sim_name"] = sim_name
-        update_dict["car_name"] = car_name
-        update_dict["track_name"] = track_name
-        update_dict["created_by"] = session["user"]
-        update_dict["date_created"] = timestampStr
-        print(timestampStr)
-        mongo.db.setups.update({"_id": ObjectId(setup_id)}, update_dict)
-        flash("Setup Successfully Updated")
+    try:
+        if session["user"]:
+            if request.method == "POST":
+                sim_name = request.args.get('sim_name', None)
+                car_name = request.args.get('car_name', None)
+                track_name = request.args.get('track_name', None)
+                print("Part 6 : ", sim_name)
+                print("Part 6 : ", car_name)
+                print("Part 6 : ", track_name)
+                param_dict_list = list(mongo.db.sim_settings_parameters.find(
+                                {"sim_name": sim_name}).sort("order_number"))
+                print("Part 6 Parameters : ", param_dict_list)
+                print("Length of Parameters List is : ", len(param_dict_list))
+                update_dict = {}
+                dateTimeObj = datetime.now()
+                # the following line of code borrowed from "https://thispointer.com/python-how-to-convert-datetime-object-to-string-using-datetime-strftime/"
+                # to get the current date and time.
+                timestampStr = dateTimeObj.strftime("%Y %m %d - %H:%M:%S")
+                print('Current Timestamp PT6 : ', timestampStr)
+                for param_dict in param_dict_list:
+                    parameter_name = param_dict["param"]
+                    update_dict[parameter_name] = request.form.get(parameter_name)
+                update_dict["sim_name"] = sim_name
+                update_dict["car_name"] = car_name
+                update_dict["track_name"] = track_name
+                update_dict["created_by"] = session["user"]
+                update_dict["date_created"] = timestampStr
+                print(timestampStr)
+                mongo.db.setups.update({"_id": ObjectId(setup_id)}, update_dict)
+                flash("Setup Successfully Updated")
+                return redirect(url_for("home"))
+            setup = mongo.db.setups.find_one({"_id": ObjectId(setup_id)})
+            print("400 : Setup = : ", setup)
+            sim_name = setup["sim_name"]
+            print("410 : sim_name = : ", sim_name)
+            headers = list(mongo.db.sim_headings.find({"sim_name": sim_name})
+                        .sort("heading_number"))
+            setup_parameters = list(mongo.db.sim_settings_parameters.find(
+                                {"sim_name": sim_name}).sort("order_number"))
+            return render_template("edit_setup.html",
+                                setup=setup, headers=headers,
+                                setup_parameters=setup_parameters)
+    except:
+        flash("Please login to edit your setups")
         return redirect(url_for("home"))
-    setup = mongo.db.setups.find_one({"_id": ObjectId(setup_id)})
-    print("400 : Setup = : ", setup)
-    sim_name = setup["sim_name"]
-    print("410 : sim_name = : ", sim_name)
-    headers = list(mongo.db.sim_headings.find({"sim_name": sim_name})
-                   .sort("heading_number"))
-    setup_parameters = list(mongo.db.sim_settings_parameters.find(
-                           {"sim_name": sim_name}).sort("order_number"))
-    return render_template("edit_setup.html",
-                           setup=setup, headers=headers,
-                           setup_parameters=setup_parameters)
 
 
 @app.route("/view_setup/<setup_id>", methods=["GET", "POST"])
