113e465446f4de0fab5106a11d45120018440247
AndyB-WHG <andrew.bond2@gmail.com>
2021-09-21 01:29:18 +0000

    style: final fine tuning.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+// Materialize initialisaton
+
 $(document).ready(function () {
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
@@ -5,25 +7,12 @@ $(document).ready(function () {
     $('select').formSelect();
 });
 
-
-// Adds a submit button to the page when a 'Sim' name is selected.
+// Adds a submit button to the 'Search' pages when a 'Sim' name is selected.
 
 $("#sim_name").on('change', function() {
     // the following line of code was found at https://www.codegrepper.com/code-examples/javascript/how+to+get+the+option+text+from+dropdown+with+option+value+javascript
     var sim_name = this.options[this.selectedIndex].text;
     console.log(sim_name);
-    document.getElementById('submit-setup-button-div').innerHTML = '<div class="col s12 center-align"><button id="submit-setup-button" type="submit" class="btn-large light-blue text-shadow">Automated Button Click<i class="fas fa-plus-square right"></i></button></div>'
+    document.getElementById('submit-setup-button-div').innerHTML = '<div class="col s12 center-align"><button id="submit-setup-button" type="submit" class="btn-large light-blue text-shadow">Automated Button Click<i class="fas fa-plus-square right"></i></button></div>';
     document.getElementById('submit-setup-button').click();
 });
-
-$("#car_name").on('change', function() {
-    // the following line of code was found at https://www.codegrepper.com/code-examples/javascript/how+to+get+the+option+text+from+dropdown+with+option+value+javascript
-    var car_name = this.options[this.selectedIndex].text;
-    console.log(car_name);
-});
-
-enterSettingsButton = document.getElementById('enter-settings-button');
-
-
-
-
